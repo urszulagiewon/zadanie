64261d329d351328a6e4e26c3846d8febc042fe9
@urszula.giewon <urszula giewon@gmail.com>
2019-03-01 17:31:15 +0100

    usunięcie zbędnych plików

--- js/main2.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-$(".next").on("click", function(){
-    $(".item:first").insertAfter(".item:last");
-});
-$(".prev").on("click", function(){
-    $(".item:last").insertBefore($(".item:first"));
-});
-});
